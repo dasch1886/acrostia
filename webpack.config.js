0e00660aa0065f2fb9c40a973d486108564f843b
Kamil Kopacz <k.dasch@interia.pl>
2019-02-25 16:28:59 +0100

    added configuraiton for services page

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,62 @@
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
-    entry: {
-        "index": [__dirname + '/src/app.js', __dirname + '/src/scss/home/style.scss']
-    },
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js',
-    },
-    optimization: {
-        minimize: true,
-    },
-    devServer: {
-        contentBase: __dirname + '/dist',
-        port: 9000
-    },
-    module: {
-        rules: [{
-                test: /\.(png|jpg|gif)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: '/images',
-                    }
-                }]
-            },
-            {
-                test: /\.scss$/,
-                use: [{
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [
-                                autoprefixer()
-                            ]
-                        },
-                    },
-                    {
-                        loader: 'sass-loader',
-                    }
-                ]
+  entry: {
+    home: [
+      __dirname + '/src/app/home.js',
+      __dirname + '/src/scss/home/style.scss'
+    ],
+    services: [
+      __dirname + '/src/app/services.js',
+      __dirname + '/src/scss/services/style.scss'
+    ]
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name]/bundle.js',
+  },
+  optimization: {
+    minimize: true
+  },
+  devServer: {
+    contentBase: __dirname + '/dist',
+    port: 9000,
+    
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: '/images'
             }
+          }
         ]
-    }
-}
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [autoprefixer()]
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
